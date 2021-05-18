16439a3c6ac964df365da041701b82c1f3eb183a
kichigina.ts <kichigina.ts@skbkontur.ru>
2021-05-18 14:02:45 +0500

    временный фикс ссылочек sana

--- NewsPlease/LinkFinders/SanaLinkFinder.cs
+++ NewsPlease/LinkFinders/SanaLinkFinder.cs
@@ -6,14 +6,14 @@ namespace NewsPlease.LinkFinders
 {
     public class SanaLinkFinder : HtmlLinkFinderBase, ILinkFinder
     {
-        private static readonly Regex Regex = new Regex(Regex.Escape("https://sana.sy/en/?p="), RegexOptions.Compiled);
+        private static readonly Regex Regex = new Regex(Regex.Escape("http://sana.sy/en/?p="), RegexOptions.Compiled);
 
         public IEnumerable<string> GetLinks()
         {
             var links = new List<string>();
             for (var i = 1; i <= 3; i++)
             {
-                var url = $"https://sana.sy/en/?paged={i}";
+                var url = $"http://sana.sy/en/?paged={i}";
                 var pageLinks = GetLinksFromPage(url).Where(x => Regex.IsMatch(x));
                 links.AddRange(pageLinks);
             }
